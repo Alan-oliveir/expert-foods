aa15d1828b36af699eea6f3b3bcf02801f371983
Alan de Oliveira Gonçalves <alandeog@poli.ufrj.br>
2024-02-14 12:19:43 -0300

    Update product and cart functionality

--- src/stores/helpers/cart-in-memory.ts
+++ src/stores/helpers/cart-in-memory.ts
@@ -1,5 +1,5 @@
-import { ProductCartProps } from '@/stores/cart-store';
-import { ProductProps } from '@/utils/data/products';
+import { ProductCartProps } from "@/stores/cart-store";
+import { ProductProps } from "@/utils/data/products";
 
 export function add(products: ProductCartProps[], newProduct: ProductProps) {
   const existingProduct = products.find(({ id }) => newProduct.id === id);
@@ -8,9 +8,22 @@ export function add(products: ProductCartProps[], newProduct: ProductProps) {
     return products.map((product) =>
       product.id === existingProduct.id
         ? { ...product, quantity: product.quantity + 1 }
-        : product,
+        : product
     );
   }
 
   return [...products, { ...newProduct, quantity: 1 }];
+}
+
+export function remove(products: ProductCartProps[], productRemovedId: string) {
+  const updatedProducts = products.map((product) =>
+    product.id === productRemovedId
+      ? {
+          ...product,
+          quantity: product.quantity > 1 ? product.quantity - 1 : 0,
+        }
+      : product
+  );
+
+  return updatedProducts.filter((product) => product.quantity > 0);
 }
