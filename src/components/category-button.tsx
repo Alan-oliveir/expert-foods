94180ccce4f2dcf5023b1b4bd98b1a3e8e358397
Alan de Oliveira Gonçalves <alandeog@poli.ufrj.br>
2024-02-14 10:20:46 -0300

    Update dependencies and fix styling issues

--- src/components/category-button.tsx
+++ src/components/category-button.tsx
@@ -1,5 +1,5 @@
-import { Pressable, PressableProps, Text } from "react-native";
-import { clsx } from "clsx";
+import { Pressable, PressableProps, Text } from 'react-native';
+import { clsx } from 'clsx';
 
 type CategoryProps = PressableProps & {
   title: string;
@@ -9,10 +9,7 @@ type CategoryProps = PressableProps & {
 export function CategoryButton({ title, isSelected, ...rest }: CategoryProps) {
   return (
     <Pressable
-      className={clsx(
-        "bg-slate-800 px-4 justify-center rounded-md h-10",
-        isSelected && "border-2 border-lime-300"
-      )}
+      className={clsx('bg-slate-800 px-4 justify-center rounded-md h-10', isSelected && "border-2 border-lime-300")}
       {...rest}
     >
       <Text className="text-slate-100 font-subtitle text-sm">{title}</Text>
